f9ee88309536e430cf0b5be040413ce1f90012ea
[YuHan] <r11621208@ntu.edu.tw>
2023-04-27 13:48:19 +0800

    rename files

--- canny.py
+++ canny.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 img = cv.imread("water_coins.jpg")
 assert img is not None , "file could not be read"
-edges = cv.Canny(img,100,200)
+edges = cv.Canny(img,180,200)
 
 plt.subplot(121),plt.imshow(img,cmap="gray")
 plt.title("Original image"),plt.xticks([]),plt.yticks([])
