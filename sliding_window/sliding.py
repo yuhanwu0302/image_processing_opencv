216d4d9bb904cb43d84da6e9fada3a4e4ffadae5
YuHan <r11621208@ntu.edu.tw>
2023-07-06 20:07:28 +0800

    modify window width and need to bilud gradient to function(yet)

--- sliding_window/sliding.py
+++ sliding_window/sliding.py
@@ -1,21 +1,26 @@
-def slidingwindow(k,arr):
+def slidingwindow(k,arr,w=1):
     start = 0
-    sum = 0.0
+    total = 0.0
+    add = k-1 
     result = []
     for end in range((len(arr))):
-        sum += arr[end]
-        if end >= k-1:
-            result.append(sum/k)
-            sum -= arr[start]
-            start += 1
+        total += arr[end]
+        if end >= add:
+            result.append(total/k)
+            add += w
+            if w != 1:
+                total -= sum(arr[start:start+w])
+            else:
+                total -= arr[start]
+            start += w
+            if start >= len(arr):  
+                break
     return list(map(int,result))
-
 
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import re
-
 
 x_li=[]
 y_li=[]
@@ -29,14 +34,21 @@ with open(r"C:\Users\baba\Desktop\image_processing_opencv\contours\contours_2_06
 x_li = list(map(int,x_li))
 y_li = list(map(int,y_li))
 
-sliding_x = slidingwindow(10,x_li)
-sliding_y = slidingwindow(10,y_li)
-
-
+sliding_x = slidingwindow(10,x_li,1)
+sliding_y = slidingwindow(10,y_li,1)
+len(sliding_x)
 #### calculate the gradient
+
+
+
+############## 要再將 gradient 寫成function !!!
+
+
+
 grad_x =[]
 grad_y =[]
-for i in np.linspace(10,667,68,dtype='int'):
+
+for i in np.linspace(10,len(sliding_x)-1,66,dtype='int'):
     diff_x = int(sliding_x[i]) - int(sliding_x[i-10])
     diff_y = int(sliding_y[i]) - int(sliding_y[i-10])
     print(diff_x,diff_y)
